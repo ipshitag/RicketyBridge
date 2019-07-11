15ff29e83518f036b4d751e202e676b00769a2d9
Ipshita Ghosh <iuippi3@gmail.com>
2019-07-11 23:28:24 +0530

    Create rickety.cpp

---
+++ rickety.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+ int arr1[6],arr2[6],f1,f2,l1,l2;
+ //arr1 = array for waiting
+ //arr2 = array for reached
+ //f1 = front flag for arr1
+ //l1 = end flag for arr1
+ //f2 = front tab for arr2
+ //l2 = end tag for arr2
