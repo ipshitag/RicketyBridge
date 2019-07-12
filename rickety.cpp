c19065771eb2ed39b5a2acd0679f0e271fe50ddb
Ipshita Ghosh <iuippi3@gmail.com>
2019-07-12 14:06:27 +0530

    Update rickety.cpp

--- rickety.cpp
+++ rickety.cpp
@@ -20,78 +20,87 @@ void backWait(int);
 
  //time = for calculating total time
 
-//decisionFunc = A function to see whether its the last pair waiting to be crossed or not.
+//decisionFunc = A function to send 2 pairs to the the function and increasing the f1
+//toReached = A function to send the pair to the reached queue(insert), and sending the first one to backWait, and also to check whether all of them have reached, i.e. whether the length of arr2 is 4
+//backWait = A function to insert the 1st person in reached(arr2) back to waiting, i.e. this function is used for the person who carries the torch back
 
 
 void decisionFunc()
 {
- toReached(arr1[f1],arr1[f1+1]);
- f1 = f1+2;
+   //Sending first 2 elements to toReached
+   toReached(arr1[f1],arr1[f1+1]);
+   f1 = f1+2;
 }
+
 
 void toReached(int a, int b)
 {
-
- if(f2 == 0 && l2 == 0)
- {
-  arr2[f2] = a;
-  arr2[f2+1] = b;
-  l2++;
-  if(a>b)
-  time = time + a;
-  else
-  time = time + b;
-
-  backWait(arr2[f2]);
-   f2++;
- }
- else
- {
-  if( (f2-l2)+1 != 4)
-  {
-   l2++;
-   arr2[l2] = a;
-   l2++;
-   arr2[l2] = b;
-
-   if(a>b)
-   time = time + a;
-   else
-   time = time + b;
-
-   if( (l2-f2)+1 != 4)
+   //checking if its the beginning of the queue
+   if(f2 == 0 && l2 == 0)
    {
-    backWait(arr2[f2]);
-    f2++;
+    arr2[f2] = a;
+    arr2[f2+1] = b;
+    l2++;
+
+   //Time is added of the one being slower
+   
+    if(a>b)
+      time = time + a;
+    else
+      time = time + b;
+ 
+   //Sending first person of reached
+   backWait(arr2[f2]);
+   f2++;
    }
+ 
+ else
+  {
+   
+    if( (f2-l2)+1 != 4)    //checking if all of them have reached or not
+     {
+      //inserting into arr2 array i.e. reached queue
+      l2++;
+      arr2[l2] = a;
+      l2++;
+      arr2[l2] = b;
+ 
+      //time of slower is added
+      if(a>b)
+       time = time + a;
+      else
+       time = time + b;
+
+     //if all of them have reached, then, we do not need to send anyone back with torch
+     if( (l2-f2)+1 != 4)
+     {
+      backWait(arr2[f2]);
+      f2++;
+     }
+    }
   }
- }
 }
+
 
 void backWait(int a)
-{
- l1++;
- arr1[l1]=a;
- time = time+a;
-}
+ {
+  //adding the element to the end of arr1 i.e. the waiting queue
+  l1++;
+  arr1[l1]=a;
+  time = time+a;
+ }
 
 
 void main()
 {
+ 
  clrscr();
-
  f1 = f2 = l2=0;
  l1 = 3;
-
-
  decisionFunc();
  decisionFunc();
  decisionFunc();
-
  cout<<time<<" minutes";
  getch();
 
 }
-
-
-
